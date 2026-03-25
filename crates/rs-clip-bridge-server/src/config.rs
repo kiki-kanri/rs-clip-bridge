48d410fb75d4cd1f9e3fe524ea32e1e86aaf6fa8
kiki-kanri <a470666@gmail.com>
2026-03-26 00:08:03 +0800

    fix(server): set config default value

--- crates/rs-clip-bridge-server/src/config.rs
+++ crates/rs-clip-bridge-server/src/config.rs
@@ -2,7 +2,13 @@ use confique::Config;
 
 #[derive(Clone, Config, Debug)]
 pub struct ServerConfig {
+    /// List of auth keys
+    #[config(default = [])]
     pub auth_keys: Vec<String>,
+
+    /// Server host
     pub host: String,
+
+    /// Server port
     pub port: u16,
 }
